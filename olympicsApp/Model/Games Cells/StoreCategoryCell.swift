dcbef99f40de61f5139895e62519d5f87c925fee
Cristiano Correia <cristiano4861@gmail.com>
2019-08-19 09:03:01 -0300

    Other store

---
+++ olympicsApp/Model/Games Cells/StoreCategoryCell.swift
@@ -0,0 +1,15 @@
+//
+//  StoreCategoryCollectionViewCell.swift
+//  olympicsApp
+//
+//  Created by Cristiano Correia on 17/08/19.
+//  Copyright © 2019 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class StoreCategoryCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    
+}
---
+++ olympicsApp/Model/Games Cells/StoreItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  StoreItemCell.swift
+//  olympicsApp
+//
+//  Created by Cristiano Correia on 17/08/19.
+//  Copyright © 2019 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class StoreItemCell: UICollectionViewCell {
+    
+}

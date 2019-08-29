ef96efad282478730bafcd7e08b0bf0f29bf1419
Carlos <modinez.carlos@gmail.com>
2019-08-29 10:09:35 -0300

    functions outfit

--- olympicsApp/Model/Outfit.swift
+++ olympicsApp/Model/Outfit.swift
@@ -10,25 +10,12 @@ import Foundation
 
 class Outfit {
     
-    var shirt : ItemStore
-    var pants : ItemStore
-//    var hats : ItemStore
-//    var glasses : ItemStore
-    
-    
-    init(shirt : ItemStore, pants : ItemStore) {
-        self.shirt = shirt
-        self.pants = pants
-    }
-    
-    func buyItem(categoryPosition : Int,  itemPosition : Int) -> Bool {
+    static func buyItem(categoryPosition : Int,  itemPosition : Int) -> Bool {
         
         let item = AllItems.shared.categories[categoryPosition].items[itemPosition]
-        
         if checkMoney(item: item) == true {
             item.bought = true
             item.inuse = true
-
             return true
         }
             
@@ -36,9 +23,25 @@ class Outfit {
             return false
         }
     }
-    func useItem
     
-    func checkMoney(item : ItemStore) -> Bool {
+    
+    static func useItem (categoryPosition : Int , itemPosition : Int) {
+        let item = AllItems.shared.categories[categoryPosition].items[itemPosition]
+        
+        if item.bought == true {
+            item.inuse = true
+            
+            //FREDDy nao pode usar dois items  de uma mesma categoria\
+            for i in AllItems.shared.categories[categoryPosition].items {
+                
+                if i.key != item.key {
+                    i.inuse = false
+                }
+            }
+        }
+    }
+    
+    static func checkMoney(item : ItemStore) -> Bool {
         if Model.instance.totalPoints >= item.value {
             return true
         }
@@ -46,4 +49,22 @@ class Outfit {
             return false
         }
     }
+    
+    
+    //Nao  está otimizado
+    static func getOutfit() -> [ItemStore] {
+        var inUseItems : [ItemStore]
+        inUseItems = []
+        
+        for index in 0...AllItems.shared.categories.count{
+            for index2 in 0...AllItems.shared.categories[index].items.count {
+                
+                if AllItems.shared.categories[index].items[index2].inuse == true {
+                    inUseItems.append(AllItems.shared.categories[index].items[index2])
+                }
+            }
+        }
+        return inUseItems
+    }
+    
 }
